82eee6427b2161d477ec9643c2383bcd5b86a993
Edgar Flores <eflorespalma@gmail.com>
2020-04-12 13:21:58 -0500

    Update main.rs

--- first-crates/src/main.rs
+++ first-crates/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{stdout, BufWriter};
 
 fn main() {
     let stdout = stdout();
-    let out = b"Hello fellow Rustaceans!";
+    let out = b"Playground with Rust!";
     let width = 24;
 
     let mut writer = BufWriter::new(stdout.lock());
